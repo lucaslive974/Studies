1ee442d66b7c32e4008506bb535a7c71db61242a
lucaslive974 <lucasribeirolima974@gmail.com>
2026-08-19 19:24:10 -0300

    [feat]: Fast exponentiation cleanup.

--- studies/src/algorithms/fastExponentiation.cpp
+++ studies/src/algorithms/fastExponentiation.cpp
@@ -5,14 +5,11 @@
 
 inline bool isPowerOfTwo(unsigned int num) { return (num > 0 && (num & (num - 1)) == 0); }
 
-unsigned int algorithms::numeric::fastExponentiation(unsigned int num, unsigned int expo) {
-    if (isPowerOfTwo(num))
-        return powerOfTwoExponentiation(num, expo);
-
+unsigned int fastExponentiationRecursive(unsigned int num, unsigned int expo) {
     if (expo == 0)
         return 1;
 
-    unsigned int half = fastExponentiation(num, expo / 2);
+    unsigned int half = fastExponentiationRecursive(num, expo / 2);
     half *= half;
 
     if (expo & 1)
@@ -21,8 +18,17 @@ unsigned int algorithms::numeric::fastExponentiation(unsigned int num, unsigned 
     return half;
 };
 
+unsigned int algorithms::numeric::fastExponentiation(unsigned int num, unsigned int expo) {
+    if (isPowerOfTwo(num)) {
+        std::cout << "Power of 2 Bitshift Exponentiation" << '\n';
+        return algorithms::numeric::powerOfTwoExponentiation(num, expo);
+    }
+
+    std::cout << "Fast exponentiation" << '\n';
+    return fastExponentiationRecursive(num, expo);
+}
+
 unsigned int algorithms::numeric::powerOfTwoExponentiation(unsigned int num, unsigned int expo) {
     assert(isPowerOfTwo(num) && "Number must be a power of two to be bitshift exponentiated.");
-    std::cout << "Power of 2 Bitshift Exponentiation" << '\n';
     return 1 << (std::countr_zero(num) * expo);
 }
--- studies/src/tests/algorithms.cpp
+++ studies/src/tests/algorithms.cpp
@@ -7,9 +7,14 @@ int main(int argc, char **argv) {
     std::vector<int> vec{4, 9, 1, 8, 4, 3};
     sorting::mergeSort(vec);
 
-    std::cout << "Power of two expo: 2¹⁰=" << numeric::fastExponentiation(2, 10) << '\n';
-    std::cout << "Power of two expo: 4²=" << numeric::fastExponentiation(4, 2) << '\n';
-    std::cout << "Fast exponentiation: 3³=" << numeric::fastExponentiation(3, 3) << '\n';
+    unsigned int res1 = numeric::fastExponentiation(2, 10) << '\n';
+    std::cout << "2¹⁰=" << res1 << '\n';
+
+    unsigned int res2 = numeric::fastExponentiation(4, 2) << '\n';
+    std::cout << "4²=" << res2 << '\n';
+
+    unsigned int res3 = numeric::fastExponentiation(3, 3) << '\n';
+    std::cout << "3³=" << res3 << '\n';
 
     return 0;
 }
